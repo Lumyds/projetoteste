82eba120da8535f52bda1448fae569bfb4ba67ec
Aluno <Aluno@LAB04>
2019-07-29 17:12:08 -0300

    primeiro commit

--- index.php
+++ index.php
@@ -7,11 +7,23 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Twitter clone</title>
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script>
+            // codigo javascript
+            </script>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">  
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle colapsed" data-toggle="collapse" data-target="#navbar" aroa-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span> 
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>           
+                    </button>
+                    <img src="imagens/twitter-logo-silhouette.png" width="100px" height="100 px"/>
     </body>
 </html>
