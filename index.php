e04abdeb3b0ced3a71f78d0219a393cbb2d1103b
Aluno <Aluno@LAB04>
2019-07-31 15:16:43 -0300

    primeiro commit

--- index.php
+++ index.php
@@ -25,5 +25,37 @@ and open the template in the editor.
                         <span class="icon-bar"></span>           
                     </button>
                     <img src="imagens/twitter-logo-silhouette.png" width="100px" height="100 px"/>
-    </body>
-</html>
+                    <div id="navbar" class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="inscrevase.php">Inscrever-se</a></li>  
+                            <li class="">
+                                <a id="entrar" data-target="#" href="#" data-toggçle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Entrar</a>
+                                <ul class="dropdown-menu" aria-labelledby="entrar">
+                                    <div class="col-md-12">
+                                        <p>Você possui uma conta?</h3>
+                                            <br />
+                                        <form method="post" action="" id="dormLogin">
+                                          <div class="form-group">
+                                              <input type="text" class="form-control" id="campo_usuario" name="usuario" placeholder="Usuário"/>
+                                          </div>
+                                            <div class="form-group">
+                                                <input type="password" class="form-control red" id="campo_senha" name="senha" placeholder="Senha"/>
+                                          </div>
+                                            <button type="buttom" class="btn btn-primary" id="btn_login">Entrar</button>
+                                            <br /><br />
+                                        </form>
+                                        </form>
+                                </ul>
+                            </li>
+                    </ul>
+</div>
+                                    </div>
+                    </nav>
+        <div class="container">
+            <div class="jumbotron">
+                <h1>Bem vindo ao twitter clone</h1>
+                <p>Veja o que está acontecendo agora...</p>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+                                        </html>
